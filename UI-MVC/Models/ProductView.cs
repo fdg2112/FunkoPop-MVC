b6d548f65543cb91aef0932e9335d25770fb9251
fdg2112 <fdg2112@gmail.com>
2023-09-04 18:26:35 -0300

    MVC creado

---
+++ UI-MVC/Models/ProductView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UI_MVC.Models
+{
+    public class ProductView
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Catalog_number { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public bool Shine { get; set; }
+        public int? Collection_id { get; set; }
+        public bool Active { get; set; }
+        public string Url_image { get; set; }
+        public string Ref_image { get; set; }
+    }
+}
